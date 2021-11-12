e51f8a38a1754fd8683fba6c40bdb552b0b8a747
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-12 16:48:38 +0300

    search bar feature

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
 const express = require("express");
+const app = express();
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const axios=require("axios");
+const axios = require("axios");
 dotenv.config();
+app.use(express.json());
+
+let songs = [];
+let movies = [];
+app.get("/movies", (req, res) => {
+  axios
+    .get("https://itunes.apple.com/search?term=all&media=movie")
+    .then((response) => {
+      movies = [...response.data.results];
+      // console.log(movies);
+      res.status(200).json(response.data);
+    });
+});
+
+app.get("/songs", (req, res) => {
+  axios
+    .get("https://itunes.apple.com/search?term=all&media=music")
+    .then((response) => {
+      songs = [...response.data.results];
+      //  console.log(songs);
+      res.status(200).json(response.data);
+    });
+  console.log(songs);
+});
+
+//// Search Function
+app.get("/search", (req, res) => {
+  const searchVal = req.body.value;
+
+  axios
+    .get("https://itunes.apple.com/search?term=all&media=music")
+    .then((response) => {
+      let result = response.data.results.filter((item) => {
+        return (
+          item.trackName.toLowerCase() == searchVal.toLowerCase() ||
+          item.artistName.toLowerCase() == searchVal.toLowerCase()
+        );
+
+        // console.log(ele);
+      });
+      res.status(200).json(result);
+    });
+});
 
 
-app.get("/movies",(req,res)=>{
-  axios.get("https://itunes.apple.com/search?term=all&media=movie")
-  .then((response)=>{
-      res.status(200).json(response.data)
-  })
-})
-app.get("/songs",(req,res)=>{
-  axios.get("https://itunes.apple.com/search?term=all&media=music")
-  .then((response)=>{
-      res.status(200).json(response.data)
-  })
-})
 
-
-const PORT =  process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server on ${PORT}`);
 });
-
